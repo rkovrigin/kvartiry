7d23aa80d8606b0d2d204beff3482fb5e4a998ca
rkovrigin <romakovrigin@gmail.com>
2013-06-25 23:49:20 +0400

    first sample

---
+++ kvartiry.py
@@ -0,0 +1,21 @@
+import urllib
+from lxml import html
+
+url = "http://www.naidi-kvartiru.ru/search?rooms=2&city=%D0%9C%D0%BE%D1%81%D0%BA%D0%B2%D0%B0&keywords=&sort=0&photo=0&page="
+
+page = html.fromstring(urllib.urlopen(url + '1').read())
+
+links = page.xpath("//html/body/div[2]/div[h5/text()!='']")
+
+
+# /html/body/div[2]/div[2]/h5/text()
+# /html/body/div[2]/div[3]/h3/a
+# 'lxml.html.HtmlElement'
+print len(links)
+
+for i in links:
+	x = i.xpath("h3/a")
+	y = x[0]
+	line = y.text
+	line = line.replace('\t','')
+	print html.tostring(line)
