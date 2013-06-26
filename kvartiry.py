edea43a33ae2af3a080ea75aea9d7e2df2eb9b5d
rkovrigin <romakovrigin@gmail.com>
2013-06-26 23:52:38 +0400

    2

--- kvartiry.py
+++ kvartiry.py
@@ -1,4 +1,5 @@
 import urllib
+import codecs
 from lxml import html
 
 url = "http://www.naidi-kvartiru.ru/search?rooms=2&city=%D0%9C%D0%BE%D1%81%D0%BA%D0%B2%D0%B0&keywords=&sort=0&photo=0&page="
@@ -14,8 +15,8 @@ links = page.xpath("//html/body/div[2]/div[h5/text()!='']")
 print len(links)
 
 for i in links:
-	x = i.xpath("h3/a")
-	y = x[0]
-	line = y.text
-	line = line.replace('\t','')
-	print html.tostring(line)
+	x = (i.xpath("h3/a"))[0]
+	line = x.text
+	# line = line.replace('\t','')
+	
+	print line.decode('utf-8')
