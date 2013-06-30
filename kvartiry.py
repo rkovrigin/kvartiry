346bebc242f3172d1b819dc25b54e602098ed57e
rkovrigin <romakovrigin@gmail.com>
2013-06-30 19:12:49 +0400

    30.06.13

--- kvartiry.py
+++ kvartiry.py
@@ -7,25 +7,19 @@ url = "http://www.naidi-kvartiru.ru/search?rooms=2&city=%D0%9C%D0%BE%D1%81%D0%BA
 page = html.fromstring(urllib.urlopen(url + '1').read())
 links = page.xpath("//html/body/div[2]/div[h5/text()!='']")
 
+
+# cian xpath //*[@id="dl2m_9745552_metro"]/a[3]
+#//*/id/a[3]
+#//*[@id="dl2m_9649395_metro"]/a[3]
 urlCian = "http://www.cian.ru/cat.php?deal_type=1&obl_id=1&metro[0]=0&city[0]=1&type=-2&wp=1&room2=1&foot_min=15&only_foot=2&cl=-1&p=1"
-for i in 300:
-	metro = "http://www.cian.ru/cat.php?deal_type=1&obl_id=1&metro[0]=" + str(i) + "&city[0]=1&type=-2&wp=1&room2=1&foot_min=15&only_foot=2&cl=-1&p=1"
+#//*[@id="tbody"]/tbody/tr/td/table/tbody/tr/td/div[1]/fieldset/table[2]/tbody/tr/
 
-# "room2=1" - 2х комнатная
-# "room1=1" - 1 комнатная
+for i in range(300):
+	metro = "http://www.cian.ru/cat.php?deal_type=1&obl_id=1&metro[0]=" + str(i+1) + "&city[0]=1&type=-2&wp=1&room2=1&foot_min=15&only_foot=2&cl=-1&p=1"
+	page = html.fromstring(urllib.urlopen(metro).read())
+	link = page.xpath("//*[@*]/a[3]")
+	if (len(link) > 0 ):
+		print "%d %s" % (i+1, link[0].text)
 
+	
 
-
-# /html/body/div[2]/div[2]/h5/text()
-# /html/body/div[2]/div[3]/h3/a
-# 'lxml.html.HtmlElement'
-#print len(links)
-# line = "";
-# for i in links:
-# 	x = i.xpath("h3/a")
-# 	y = x[0]
-# 	line = y.text
-# 	line2 = line.encode('latin1', 'ignore')
-# 	line2 = line2.replace("\r\n", "")
-# 	print line2.replace("\xc3", "")
-#print unicodedata.normalize('NFKD', line)
